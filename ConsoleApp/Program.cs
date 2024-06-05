925117cc383f17f0137acc912deb9fc5bb55f900
Mickey <zmisa0053@gmail.com>
2024-06-06 01:30:17 +0500

    Download consoleapp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RPN.Logika;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите математическое выражение:");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Введите значение переменной x:");
+        if (!double.TryParse(Console.ReadLine(), out double variableValue))
+        {
+            Console.WriteLine("Ошибка: неверное значение переменной x.");
+            return;
+        }
+
+        try
+        {
+            var rpn = InfixToRPNConverter.Convert(input);
+            Console.WriteLine("\nОбратная польская запись (ОПЗ):");
+            foreach (var token in rpn)
+            {
+                Console.Write(token + " ");
+            }
+            Console.WriteLine();
+
+            double resultRPN = RPNCalculator.Calculate(rpn, variableValue);
+            Console.WriteLine("\nРезультат вычисления ОПЗ:");
+            Console.WriteLine(resultRPN);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
